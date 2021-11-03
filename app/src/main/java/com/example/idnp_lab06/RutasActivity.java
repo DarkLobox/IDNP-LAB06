9ca69f98d1df2d3cba574cd62a79dde33ec74fc5
DarkLobox <57413922+DarkLobox@users.noreply.github.com>
2021-11-02 23:28:49 -0600

    RutasActivity

--- app/src/main/java/com/example/idnp_lab06/RutasActivity.java
+++ app/src/main/java/com/example/idnp_lab06/RutasActivity.java
@@ -2,23 +2,51 @@ package com.example.idnp_lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RutasActivity extends AppCompatActivity {
-
+    String llave = "coordenadas";
+    TextView co;
+    EditText la,lo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rutas);
+        co = findViewById(R.id.coordenadasGuardadas);
+        la = findViewById(R.id.inputLatitudRuta);
+        lo = findViewById(R.id.inputLongitudRuta);
+
+        SharedPreferences preferences = getSharedPreferences("ruta",Context.MODE_PRIVATE);
+        co.setText(preferences.getString(llave,""));
     }
 
-    public void onClickBorrarRuta(View view){
-
+    public void BorrarRuta(View view){
+        SharedPreferences preferences = getSharedPreferences("ruta",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(llave,"");
+        editor.apply();
+        la.setText("");
+        lo.setText("");
+        co.setText("");
     }
 
-    public void onClickGuardarRuta(View view){
+    public void GuardarRuta(View view){
+        SharedPreferences preferences = getSharedPreferences("ruta",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
 
+        String coordenada = la.getText().toString()+","+lo.getText().toString();
+        String valorActual = preferences.getString(llave,"") + "\n" +coordenada;
+        editor.putString(llave,valorActual);
+        editor.apply();
+
+        la.setText("");
+        lo.setText("");
+        co.setText(valorActual);
     }
 
 }
