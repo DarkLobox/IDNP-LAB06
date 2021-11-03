c168e97b4581ae2ee8bcb72f0cf367788b556bad
DarkLobox <lgomezp@unsa.edu.pe>
2021-11-02 22:14:49 -0500

    Interfaces

---
+++ app/src/main/java/com/example/idnp_lab06/CalendarioActivity.java
@@ -0,0 +1,19 @@
+package com.example.idnp_lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CalendarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario);
+    }
+
+    public void onClickGuardarCalendario(View view){
+
+    }
+}
---
+++ app/src/main/java/com/example/idnp_lab06/RutasActivity.java
@@ -0,0 +1,24 @@
+package com.example.idnp_lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RutasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rutas);
+    }
+
+    public void onClickBorrarRuta(View view){
+
+    }
+
+    public void onClickGuardarRuta(View view){
+
+    }
+
+}
